16b3ae0f0789c9f4b6e2d3e48e1f8927a6a64452
devonosh <devonosh@gmail.com>
2018-04-12 15:24:45 -0500

    testing exclude
    This change should be excluded.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 (function() {
 	alert("HI!!!");
-	console.log("Made a new change in the branch.");
-	console.log("Addign another line to my change.")
+	console.log("This change should be excluded.");
 })();
